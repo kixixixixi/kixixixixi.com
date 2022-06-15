8b9a217967b140762d37540c7517374568796bfb
kixixixixi <kixixixixi@gmail.com>
2022-06-15 17:15:01 +0900

    Init

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { NextPage } from "next"
+
+const IndexPage: NextPage = () => {
+  return (
+    <>
+      <h1>Welcome to kixixixixi&rsquo;s page</h1>
+    </>
+  )
+}
+
+export default IndexPage
