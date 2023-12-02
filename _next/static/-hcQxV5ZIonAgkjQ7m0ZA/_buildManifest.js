7e2413c4d15cf30c740869692f61f16f039295bb
kixixixixi <kixixixixi@users.noreply.github.com>
2023-12-02 21:34:37 +0000

    deploy: b3a497c3e6320dbd2d60a57b90b61f79f8921079

--- _next/static/-hcQxV5ZIonAgkjQ7m0ZA/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-3dd026ac9e4f86ef.js"],"/_error":["static/chunks/pages/_error-815e492bede44f3e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
