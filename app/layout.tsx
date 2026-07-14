d1f8fb79ad5c1a10a449fdeb95975eaf982c3111
kixixixixi <kixixixixi@gmail.com>
2026-07-14 19:45:55 +0900

    Set user scalable is no

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         <meta charSet="utf-8" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+          content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"
         />
         <title>kixixixixi</title>
       </head>
